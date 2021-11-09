dc0c8868d8ac1d499a11ea8accd6bb551149e904
TJ Mahr <tristan.mahr@wisc.edu>
2021-11-09 14:24:31 -0600

    work on toks

--- inst/make-solarized-theme.R
+++ inst/make-solarized-theme.R
@@ -81,38 +81,38 @@ new <- j %>%
     # I'm having trouble testing these
     line = p$base1, line_background = p$base2
   ) %>%
-  set_text_style("Comment", text = p$base01, italic = TRUE) %>%
-  # not sure that this is
-  set_text_style("CommentVar", text = p$base01, italic = TRUE) %>%
-  ## comments
-  set_text_style("Documentation", text = p$base01, italic = TRUE, bold = FALSE) %>%
-  set_text_style("Keyword", text = p$green) %>%
-  set_text_style("Variable", text = p$green) %>%
-  set_text_style("Constant", text = p$cyan) %>%
-  set_text_style("String", text = p$cyan) %>%
-  set_text_style("Float", text = p$magenta) %>%
+  set_text_style("Comment", text = p$base1, italic = TRUE) %>%
+  # not sure what this is
+  # ## comments
+  set_text_style("Documentation", text = p$base1,  italic = TRUE,  bold = FALSE) %>%
+  set_text_style("CommentVar", text = p$base1, italic = TRUE) %>%
+  # #> comments
+  set_text_style("Information",   text = p$base00, italic = FALSE, bold = FALSE) %>%
+  set_text_style("Keyword",  text = p$green) %>%
+  set_text_style("Variable", text = p$violet) %>%
+  set_text_style("Constant", text = p$orange) %>%
+  set_text_style("Float",    text = p$magenta) %>%
+  set_text_style("BaseN",    text = p$magenta) %>%
+  set_text_style("DecVal",   text = p$magenta) %>%
   set_text_style("Operator", text = p$yellow) %>%
   set_text_style("Function", text = p$blue) %>%
-  set_text_style("ControlFlow", text = p$green) %>%
-  set_text_style("Other", ) %>%
+  set_text_style("ControlFlow", text = p$green, bold = FALSE) %>%
+  set_text_style("Char",           text = p$cyan) %>%
+  set_text_style("SpecialChar",    text = p$cyan) %>%
+  set_text_style("String",         text = p$cyan) %>%
+  set_text_style("VerbatimString", text = p$cyan) %>%
+  set_text_style("SpecialString",  text = p$cyan) %>%
+  set_text_style("BuiltIn",  ) %>%
+  set_text_style("Extension", ) %>%
+  set_text_style("Preprocessor", ) %>%
   set_text_style("Attribute", ) %>%
-  set_text_style("SpecialString", ) %>%
   set_text_style("Annotation", ) %>%
-  set_text_style("BaseN", ) %>%
+  set_text_style("Other", text = p$violet) %>%
+  set_text_style("Import", ) %>%
+  set_text_style("DataType", ) %>%
   set_text_style("Error", text = p$orange) %>%
   set_text_style("Alert", text = p$orange) %>%
   set_text_style("Warning", text = p$orange) %>%
-  set_text_style("BuiltIn", ) %>%
-  set_text_style("Extension", ) %>%
-  set_text_style("Preprocessor", ) %>%
-  # i'm using this for #> comments
-  set_text_style("Information", text = p$base01, italic = FALSE, bold = FALSE) %>%
-  set_text_style("VerbatimString", text = p$base00) %>%
-  set_text_style("Import", ) %>%
-  set_text_style("Char", ) %>%
-  set_text_style("DataType", ) %>%
-  set_text_style("SpecialChar", ) %>%
-  set_text_style("DecVal", ) %>%
   { }
 
 jsonlite::write_json(
@@ -122,6 +122,82 @@ jsonlite::write_json(
   auto_unbox = TRUE
 )
 #
-# rmarkdown::render("README.Rmd")
-# system2("open", "README.docx")
+rmarkdown::render("syntax-test.Rmd")
+system2("open", "syntax-test.docx")
 # ?rmarkdown::word_document
+
+
+# Definitions of the things
+# https://docs.kde.org/stable5/en/kate/katepart/highlight.html
+#
+# General default styles:
+#
+# dsNormal, when no special highlighting is required.
+#
+# dsKeyword, built-in language keywords.
+#
+# dsFunction, function calls and definitions.
+#
+# dsVariable, if applicable: variable names (e.g. $someVar in PHP/Perl).
+#
+# dsControlFlow, control flow keywords like if, else, switch, break, return,
+# yield, ...
+#
+# dsOperator, operators like + - * / :: < >
+#
+# dsBuiltIn, built-in functions, classes, and objects.
+#
+# dsExtension, common extensions, such as Qt™ classes and functions/macros in
+# C++ and Python.
+#
+# dsPreprocessor, preprocessor statements or macro definitions.
+#
+# dsAttribute, annotations such as @override and __declspec(...). String-related
+# default styles:
+#
+# dsChar, single characters, such as 'x'.
+#
+# dsSpecialChar, chars with special meaning in strings such as escapes,
+# substitutions, or regex operators.
+#
+# dsString, strings like "hello world".
+#
+# dsVerbatimString, verbatim or raw strings like 'raw \backlash' in Perl,
+# CoffeeScript, and shells, as well as r'\raw' in Python.
+#
+# dsSpecialString, SQL, regexes, HERE docs, LATEX math mode, ...
+#
+# dsImport, import, include, require of modules. Number-related default styles:
+#
+# dsDataType, built-in data types like int, void, u64.
+#
+# dsDecVal, decimal values.
+#
+# dsBaseN, values with a base other than 10.
+#
+# dsFloat, floating point values.
+#
+# dsConstant, built-in and user defined constants like PI. Comment and
+# documentation-related default styles:
+#
+# dsComment, comments.
+#
+# dsDocumentation, /** Documentation comments */ or """docstrings""".
+#
+# dsAnnotation, documentation commands like @param, @brief.
+#
+# dsCommentVar, the variable names used in above commands, like "foobar" in
+# @param foobar.
+#
+# dsRegionMarker, region markers like //BEGIN, //END in comments. Other default
+# styles:
+#
+# dsInformation, notes and tips like @note in doxygen.
+#
+# dsWarning, warnings like @warning in doxygen.
+#
+# dsAlert, special words like TODO, FIXME, XXXX.
+#
+# dsError, error highlighting and wrong syntax.
+#
+# dsOthers, when nothing else fits.
