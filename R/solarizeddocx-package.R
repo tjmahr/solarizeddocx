b07127eee3f8e6ef16caacf95e0b1bc8421c49df
TJ Mahr <tjmahr@users.noreply.github.com>
2021-11-10 11:49:56 -0600

    prepare package

--- R/solarizeddocx-package.R
+++ R/solarizeddocx-package.R
@@ -5,43 +5,57 @@
 ## usethis namespace: end
 NULL
 
-#' Custom RMarkdown Docx Format
+#' Solarized Syntax Highlighting in Microsoft Word
+#'
+#' @param ... Arguments passed to [rmarkdown::word_document()].
+#' @param reference_docx Reference docx file to use. By default, this value is
+#'   set to use `"solarized"` to use this package's file. Alternatively, one
+#'   could `"default"` for pandoc's default file or a path to a custom docx
+#'   file.
+#' @param highlight Syntax highlighting style to use. By default, this value is
+#'   set to `"solarized"` to use this package's syntax definition. See
+#'   alternatives in [rmarkdown::word_document()].
+#' @param syntax_definition Syntax definition to use for syntax highlighting.
+#'   Defaults to `"solarized"` to use this package's language definition. This
+#'   option is not one available to  [rmarkdown::word_document()], but setting
+#'   this argument to `NULL` or `"default"` will use the default behavior.
+#' @param pandoc_args Additional command line options to pass to pandoc. (This
+#'   should be a character vector of options.)
+#' @return An RMarkdown output format for Word documents.
 #' @export
+#' @examples
+#' \dontrun{
+#' rmarkdown::render("test.Rmd", output_format = solarizeddocx::document())
+#' }
 document <- function(
   ...,
   reference_docx = "solarized",
   highlight = "solarized",
+  syntax_definition = "solarized",
   pandoc_args = character(0)
 ) {
+
   if (reference_docx == "solarized") {
-    reference_docx = pkg_resource("solarized-light.docx")
+    reference_docx = file_reference_docx()
   }
 
-  if (highlight == "solarized") {
-    style_args <- c(
-      "--highlight-style",
-      pkg_resource("solarized_light.theme")
-    )
+  style_args <- if (highlight == "solarized") {
+    c("--highlight-style", file_solarized_light_theme())
   } else {
-    style_args <- rmarkdown::pandoc_highlight_args(highlight)
+    rmarkdown::pandoc_highlight_args(highlight)
   }
 
+  syntax_args <- if (syntax_definition == "solarized") {
+    c("--syntax-definition", file_syntax_definition())
+  } else if (syntax_definition == "default") {
+    character(0)
+  } else if (is.null(syntax_definition)) {
+    character(0)
+  } else {
+    c("--syntax-definition", syntax_definition)
+  }
 
-  # pandoc_args:
-  #   - "--highlight-style=./inst/test.theme"
-  # - "--syntax-definition=./inst/r.xml"
-  # md_extensions: +fenced_code_attributes
-  # reference_docx: "inst/custom-reference.docx"
-  #
-
-  # rmarkdown::pandoc("name", "value")
-  # rmarkdown::pandoc_metadata_arg("name", "value")
-
-  args <- c(
-    "--syntax-definition", pkg_resource("r.xml"),
-    style_args
-  )
-
+  args <- c(style_args, syntax_args)
 
   rmarkdown::word_document(
     ...,
@@ -50,15 +64,61 @@ document <- function(
   )
 }
 
-demo <- function() {
+
+#' Render and open the demo RMarkdown file
+#' @export
+#' @return This function renders the built-in RMarkdown file and opens it. It
+#'   returns the path to the rendered file invisibly.
+demo_document <- function() {
   path_out <- tempfile(fileext = ".docx")
+
   rmarkdown::render(
-    input = pkg_resource("syntax-test.Rmd"),
+    file_syntax_test(),
     output_file = path_out
   )
+  system2("open", path_out)
+  invisible(path_out)
+}
+
+
+#' Retrieve file paths to package resources
+#' @name fileresources
+#' @rdname fileresources
+#' @return `file_reference_docx()` returns the path to the reference docx file
+#'   used by this package. `file_syntax_definition()` returns the path to the
+#'   XML file specifying how pandoc should highlight the code in the document.
+#'   `file_solarized_light_theme()` returns the path to the .theme file used by
+#'   this package. `file_syntax_test()` returns the path to the RMarkdown file
+#'   used by [demo_document()].
+NULL
+
+#' @rdname fileresources
+#' @export
+file_reference_docx <- function() {
+  pkg_resource("solarized-light.docx")
 
 }
 
-pkg_resource = function(...) {
+#' @rdname fileresources
+#' @export
+file_syntax_definition <- function() {
+  pkg_resource("r.xml")
+}
+
+#' @rdname fileresources
+#' @export
+file_solarized_light_theme <- function() {
+  pkg_resource("solarized_light.theme")
+}
+
+
+#' @rdname fileresources
+#' @export
+file_syntax_test <- function() {
+  pkg_resource("syntax-test.Rmd")
+}
+
+
+pkg_resource <- function(...) {
   system.file(..., package = "solarizeddocx")
 }
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
 # solarizeddocx
-A refence docx file for pandoc with solarized syntax highlighting
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+<img src="man/figures/side-by-side-2.png" title="Comparison of the solarized document theme versus the default theme." alt="Comparison of the solarized document theme versus the default theme." width="100%" />
+
+solarizeddocx provides an [RMarkdown](https://rmarkdown.rstudio.com/)
+output format to create a docx file with
+[solarized](https://github.com/altercation/solarized) syntax
+highlighting. It works by provide custom style files to
+[pandoc](https://pandoc.org/MANUAL.html).
+
+Currently only the light solarized theme is supported.
+
+## Installation
+
+You can install the development version of solarizeddocx from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("tjmahr/solarizeddocx")
+```
+
+## Usage
+
+This format can be used via YAML metadata.
+
+    output: 
+      solarizeddocx::document: default
+
+Or explicitly with rmarkdown:
+
+``` r
+rmarkdown::render(
+  "README.Rmd", 
+  output_format = solarizeddocx::document()
+)
+```
+
+## Customization
+
+The built-in demo function will render a vignette detailing all of the
+customizations used by the package.
+
+``` r
+solarizeddocx::demo_document()
+```
